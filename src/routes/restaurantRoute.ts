51d9429e2f283e8c43d670ec7b0d21ec1ea06541
FatimatuzZahro <f56446473@gmail.com>
2026-07-07 18:29:48 +0700

    update restaurantRoute

--- src/routes/restaurantRoute.ts
+++ src/routes/restaurantRoute.ts
@@ -12,12 +12,16 @@ import { authenticate, authorize } from "../midlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllRestaurants); //menampilkan data restauran
-router.post("/", authenticate, authorize("RESTAURANT_ADMIN"), createRestaurant); //buat resto
-router.get("/:id", getRestaurantById); //menampilkan resto by  id
-router.put("/:id", authenticate, authorize("RESTAURANT_ADMIN"), updateRestaurant); //mengupdate data resto by id
-router.delete("/:id", authenticate, authorize("RESTAURANT_ADMIN"), deleteRestaurant); //menghapus data resto by id
+router.get("/", getAllRestaurants);
+router.post("/", authenticate, authorize("RESTAURANT_ADMIN"), createRestaurant);
+
+
 router.get("/me", authenticate, authorize("RESTAURANT_ADMIN"), getMyRestaurant);
 router.get("/dashboard", authenticate, authorize("RESTAURANT_ADMIN"), getDashboardRestaurant);
+
+
+router.get("/:id", getRestaurantById);
+router.put("/:id", authenticate, authorize("RESTAURANT_ADMIN"), updateRestaurant);
+router.delete("/:id", authenticate, authorize("RESTAURANT_ADMIN"), deleteRestaurant);
 
 export default router;
